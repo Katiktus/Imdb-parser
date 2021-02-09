64663be9136df04b1171c0fdf2330d5f049a23fe
Kate <60921902+Katiktus@users.noreply.github.com>
2021-02-09 09:41:05 +0200

    template for task

---
+++ parse/src/main/java/ua/edu/sumdu/j2ee/pohorila/parse/ParseApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.sumdu.j2ee.pohorila.parse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParseApplication.class, args);
+    }
+
+}
